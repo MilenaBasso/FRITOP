112d2f3fb13f712ad51ad8336b289484824a1270
Isabila2 <125681801+Isabila2@users.noreply.github.com>
2023-05-17 12:35:30 -0300

    Código dos sabores

---
+++ sabores.js
@@ -0,0 +1,18 @@
+<form class="sabor">
+<select name="linguagem" required="required">
+
+  <option value="">Sabores</option>
+  <option>Cebola e salsa</option>
+  <option>Cheddar e bacon</option>
+  <option>Churrasco</option>
+  <option>Costela de porco com limão</option>
+  <option>Frango grelhado</option>
+  <option>Peito de Peru</option>
+  <option>Picanha com Alho</option>
+  <option>Requeijão</option>
+  <option>Tradicional</option>
+</select>
+
+<button type="submit">Enviar</button>
+
+</form>
