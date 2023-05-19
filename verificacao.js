a6a92f0ff69444c1c6389b7bb5f1b1fd60658579
PauloPZ <pc.zuit@gmail.com>
2023-05-18 21:56:52 -0300

    Mudar Cor teste javascript

---
+++ verificacao.js
@@ -0,0 +1,20 @@
+function verdadeiro(event) {
+  event.preventDefault();
+  var senha = 1234;
+  if(senha == true) {
+    window.prompt("oi")
+
+  }  
+}
+
+
+function mudarcor() {
+    var elemento = document.getElementById("elementomeu")
+
+    if (verdadeiro) {
+        element.style.backgroundColor = "green";
+      } else {
+        element.style.backgroundColor = "red";
+      }
+}
+
